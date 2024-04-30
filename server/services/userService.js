68ea701c776c56f8602bb0ca00819192ee6bfb52
Kim Kyungha <kimmaha9902@gmail.com>
2024-04-30 18:56:39 +0900

    사용자 정보조회 업데이트

--- server/services/userService.js
+++ server/services/userService.js
@@ -27,4 +27,39 @@ class userAuthService {
 
         return loginUser;
     }
+
+
+    // 모델 층의 User의 email 함수를 사용하여 사용자를 검색
+    static async getUserInfo({ email }) {
+         const user = await userModel.findById(email);
+    
+        if (!user) {
+            const errorMessage = "사용자를 찾을 수 없습니다."; 
+            return { errorMessage };
+        }
+
+        return user;
+    }
+
+    // 이메일 중복 확인
+    static async addUser({ email, password }) {
+        
+        const user = await User.findByEmail({ email });
+        if (user) {
+          const errorMessage =
+            "이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.";
+          return { errorMessage };
+        }
+    
+        // 비밀번호 해쉬화
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = { email, password: hashedPassword };
+    
+        // db에 저장
+        const createdNewUser = await User.create({ newUser });
+        createdNewUser.errorMessage = null; 
+    
+        return createdNewUser;
+      }
 }
