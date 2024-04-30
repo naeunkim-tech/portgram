89f3768f45a21548f79d8ece469d7d7c15e86d81
[AI10] 김나은 <naeunkim.business@gmail.com>
2024-04-30 18:53:12 +0900

    app use userRouter

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import userRouter from './routes/user.js'
 
 const app = express();
 
 app.use(express.static('views'));
 app.use(express.static('public'));
+app.use('/user', userRouter);
 
 app.get('/', (req, res) => {
     res.sendFile('views/index.html');
