e583f426042b0d81a88b082d4c077d8a0b975c78
seonaOh <osa746@gmail.com>
2024-05-01 00:06:00 +0900

    로그인 페이지 스크립트 작성

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,42 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault();
     
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
     
     console.log("Email: " + email);
     console.log("Password: " + password);
     
-    var errorMessage = document.getElementById("error-message");
+    const errorMessage = document.getElementById("error-message");
     errorMessage.textContent = "로그인실패";
+
+    const data = {
+      email,
+      password
+  };    
+
+  const options = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  };
+
+  fetch('http://localhost:5000/user/login', options)
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('로그인 실패');
+      }
+      return response.json();
+  })
+  .then(data => {
+      console.log(data);
+      window.location.href = 'personal.html';
+  })
+  .catch(error => {
+      console.error('Error:', error.message);
+      errorMessage.textContent = error.message;
+  });
   });
   
