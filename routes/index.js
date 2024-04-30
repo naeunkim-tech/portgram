71399badbe53098783dbc470135bdaeb3fe74878
yoolim <ninetale22@gmail.com>
2024-05-01 00:59:40 +0900

    에러 수정

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const projectRouter = require("./projectRouter");
+const certificateRouter = require("./certificateRouter");
+const educationRouter = require("./educationRouter");
+const awardRouter = require("./awardRouter");
+
+
+module.exports = {
+    educationRouter,
+    certificateRouter,
+    awardRouter,
+    projectRouter,
+};
