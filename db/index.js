f3c18193f02dc7fb70d97e74603e8a4e1588420b
[AI10] 김나은 <naeunkim.business@gmail.com>
2024-05-02 05:08:57 +0900

    index.js db/index.js mongoDB connect

--- db/index.js
+++ db/index.js
@@ -1,41 +1,22 @@
 const mongoose = require("mongoose");
-const { User } = require("./user.js");;// .env 환경변수 불러오기
 const dotenv = require("dotenv");
-
-dotenv.config();
-// const { User } = require("./models/user.js");
 
 require('dotenv').config(); // .env 환경변수 불러오기
 const { MONGODB_URL } = process.env;
 
-const DB_URL = process.env.MONGODB_URL || "Default MongoDB URL";
+const DB_URL = MONGODB_URL || "Default MongoDB URL";
 
 const MongoConnect = async () => {
   try {
-    await mongoose.connect(DB_URL);
+    await mongoose.connect(DB_URL); // option: { dbName: __ }
+    // mongoose.connect(DB_URL, {
+    //   dbName: 'elice_project_01',
+    // });
     console.log('MongoDB Connected...');
   } catch (error) {
     console.error('MongoDB connection error:', error);
   }
 };
 
-
 // module.exports = {AwardModel,CertificateModel,EducationModel,ProjectModel,User}
-module.exports = {User,MongoConnect}
-const DB_URL =
-  process.env.MONGODB_URL ||
-  "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.js 파일을 확인하세요.";
-
-mongoose.connect(DB_URL, {
-  dbName: 'elice_project_01',
-});
-const db = mongoose.connection;
-
-db.on("connected", () =>
-  console.log("정상적으로 MongoDB 서버에 연결되었습니다.  " + DB_URL)
-);
-db.on("error", (error) =>
-  console.error("MongoDB 연결에 실패하였습니다.\n" + DB_URL + "\n" + error)
-);
-
-// module.exports = User;
+module.exports = { MongoConnect }
