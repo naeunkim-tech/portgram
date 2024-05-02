f89d0f9b77e2f7baee84bba4b225e573068afd61
seona <osa746@gmail.com>
2024-05-02 22:55:15 +0900

    네트워크 매니저 사용하도록 수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,7 @@
+
+import {Login} from './core/networkManager.js';
+
+
 document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault();
     
@@ -8,36 +12,20 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     console.log("Password: " + password);
     
     const errorMessage = document.getElementById("error-message");
-    errorMessage.textContent = "로그인실패";
 
     const data = {
       email,
       password
   };    
 
-  const options = {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-  };
-
-  fetch('http://localhost:5000/', options)
-  .then(response => {
-      if (!response.ok) {
-          throw new Error('로그인 실패');
-      }
-    //   console.log(response);
-      return response.json();
-  })
-  .then(data => {
-      console.log(data);
-      window.location.href = 'personal.html';
-  })
-  .catch(error => {
-      console.error('Error:', error.message);
-      errorMessage.textContent = error.message;
-  });
+  Login(data, 
+    () => {
+        console.log("로그인 성공!");
+    }, 
+    () => {
+        console.log("로그인 실패!");
+        errorMessage.textContent = error.message;
+    }
+);
   });
   
--- public/js/register.js
+++ public/js/register.js
@@ -1,3 +1,5 @@
+import {Register} from './core/networkManager.js';
+
 document.addEventListener('DOMContentLoaded', function() {
     const signupForm = document.getElementById('signup-form');
     const errorMessage = document.getElementById('error-message');
@@ -25,25 +27,14 @@ document.addEventListener('DOMContentLoaded', function() {
         password,
       };    
 
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      };
-
-    fetch('http://localhost:5000/user/register', options) // localhost:5000 로컬 테스트용임. 일단
-    //TODO 서버 통신용 모듈 스크립트 따로 만들어서 fetch하는 함수 사용할 수 있도록 만들자. 
-    //로컬인지 릴리즈인지에 따라 변경되도록 해야함
-      .then(response => response.text())
-      .then(message => {
-        console.log(message);
-      })
-      .catch(error => {
-        console.error('오류 발생:', error);
-        // 오류 처리
-      });
-      
+      Register(data, 
+        () => {
+            console.log("회원가입 성공!");
+        }, 
+        () => {
+            console.log("회원가입 실패!");
+            errorMessage.textContent = error.message;
+        }
+      );
     });
   });
