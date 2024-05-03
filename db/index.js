417729766f7a9496a3eecf0ec76b77284d7ff6e7
[AI10] 김나은 <naeunkim.business@gmail.com>
2024-05-03 23:37:48 +0900

    passport added, userRouter.js userService.js app.js modified

--- db/index.js
+++ db/index.js
@@ -9,9 +9,9 @@ const DB_URL = MONGODB_URL || "Default MongoDB URL";
 const MongoConnect = async () => {
   try {
     // await mongoose.connect(DB_URL); // mongoDB 특정 데이터베이스에 연결하려면 옵션 추가, option: { dbName: __ }
-    // mongoose.connect(DB_URL, {
-    //   dbName: 'elice_project_01',
-    // });
+    mongoose.connect(DB_URL, {
+      dbName: 'elice_project_01',
+    });
     console.log('MongoDB Connected...', DB_URL);
   } catch (error) {
     console.error('MongoDB connection error:', error);
