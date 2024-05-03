1db57d1e8cfb5d458740e42a32a352f787ed1033
Code21w <167353588+Code21w@users.noreply.github.com>
2024-05-03 15:11:16 +0900

    otheruser header 요소삽입

---
+++ public/js/header.js
@@ -0,0 +1,17 @@
+var newElement = document.createElement("nav")
+newElement.className = "navbar";
+
+newElement.innerHTML = `
+    <div class="navbar_Home">
+        <a href="#">안녕하세요. 포트폴리오 공유 서비스입니다.</a>
+    </div>
+
+    <ul class="navbar_menu">
+        <li><a href="#">네트워크</a></li>
+        <li><a href="#">나의 페이지</a></li>
+        <li><a href="#">로그아웃</a></li>
+    </ul>       
+`;
+
+
+document.querySelector("#main").insertAdjacentElement("afterbegin", newElement);
