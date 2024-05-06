32f29a725e2fc7498e6e0270151a2e4bf9d1f224
[AI10] 임지영 <dlawldud021@naver.com>
2024-05-06 19:29:04 +0900

    모든 항목 버튼에 data 전송 함수 추가

--- public/js/personal.js
+++ public/js/personal.js
@@ -2,36 +2,38 @@ import { addEdu } from './personal/addEdu.js';
 import { addAward } from './personal/addAward.js';
 import { addProject } from './personal/addProject.js';
 import { addCertificate } from './personal/addCertificate.js';
+import { postData } from './personal/postData.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('addEdu').addEventListener('click', addEdu);
-  document.getElementById('education_submit').addEventListener('click', () => {
-    const form = document.getElementById('educationForm');
-    const schoolName = form.school_name.value; // form에서 학교 이름 데이터 수집
-    const majorName = form.major_name.value;
-    const optionName = form.option.value;
-
-    // 데이터 표시
-    const displayDiv = document.getElementById('addedEducation');
-    const schoolNameElement = document.createElement('p');
-    schoolNameElement.textContent = `학교 이름: ${schoolName} <br> 전공: ${majorName} <br> 최종 학력: ${optionName}`;
-    displayDiv.appendChild(schoolNameElement);
-
-    // form 초기화
-    form.reset();
+  document.getElementById('education_submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    postData('educationForm');
   });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('addAward').addEventListener('click', addAward);
+  document.getElementById('education_submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    postData('awardForm');
+  });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   document
     .getElementById('addCertificate')
     .addEventListener('click', addCertificate);
+  document.getElementById('education_submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    postData('certificateForm');
+  });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('addProject').addEventListener('click', addProject);
+  document.getElementById('education_submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    postData('projectForm');
+  });
 });
