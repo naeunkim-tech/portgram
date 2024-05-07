53dba0614210d690e4db8d46ad0b6ff7e915bf1b
[AI10] 김나은 <naeunkim.business@gmail.com>
2024-05-07 21:27:16 +0900

    db url modified

--- db/index.js
+++ db/index.js
@@ -4,15 +4,15 @@ const dotenv = require("dotenv");
 require('dotenv').config(); // .env 환경변수 불러오기
 const { MONGODB_URL } = process.env;
 
-// const DB_URL = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.5"
+const DB_URL = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.5"
 
 const MongoConnect = async () => {
   try {
     // await mongoose.connect(DB_URL); // mongoDB 특정 데이터베이스에 연결하려면 옵션 추가, option: { dbName: __ }
-    mongoose.connect(MONGODB_URL, {
+    mongoose.connect(DB_URL, {
       dbName: 'elice_project_01',
     });
-    console.log('MongoDB Connected...', MONGODB_URL);
+    console.log('MongoDB Connected...', DB_URL);
   } catch (error) {
     console.error('MongoDB connection error:', error);
   }
