d8c69daa8d570f3e25b261ad09f66ba35df199de
[AI10] 임지영 <dlawldud021@naver.com>
2024-05-08 19:02:55 +0900

    수정 버튼 땜에 변수 수정

--- public/js/personal/createInput/createTextInput.js
+++ public/js/personal/createInput/createTextInput.js
@@ -1,9 +1,11 @@
-export function createTextInput(form, name, placeholder) {
+export function createTextInput(form, name, placeholder, prevalue) {
   const input = document.createElement('input');
   input.name = name;
   input.className = 'input-style';
   input.type = 'text';
-  input.placeholder = placeholder;
+  !(placeholder === '') ? (input.placeholder = placeholder) : '';
+
+  prevalue ? (input.value = prevalue) : '';
 
   form.appendChild(input);
   form.appendChild(document.createElement('br'));
