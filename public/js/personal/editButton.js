615f670174593f35f55c79dabb4a137e436707f2
[AI10] 임지영 <dlawldud021@naver.com>
2024-05-08 23:56:24 +0900

    수정 버튼 구현 완료

--- public/js/personal/editButton.js
+++ public/js/personal/editButton.js
@@ -17,6 +17,28 @@ export function editedInfo(list, getData, info) {
   if (info === 'education') {
     createTextInput(editForm, 'school', '', getData.school);
     createTextInput(editForm, 'major', '', getData.major);
+    const degrees = ['재학중', '학사졸업', '석사졸업', '박사졸업'];
+    degrees.forEach(function (labelText, index) {
+      const radioButton = document.createElement('input');
+      radioButton.type = 'radio';
+      radioButton.id = 'option' + (index + 1);
+      radioButton.name = 'degree';
+      if (radioButton.id == 'option1') {
+        radioButton.value = '재학중';
+      } else if (radioButton.id == 'option2') {
+        radioButton.value = '학사졸업';
+      } else if (radioButton.id == 'option3') {
+        radioButton.value = '석사졸업';
+      } else {
+        radioButton.value = '박사졸업';
+      }
+
+      const label = document.createElement('label');
+      label.setAttribute('for', radioButton.id);
+      label.textContent = labelText;
+      editForm.appendChild(radioButton);
+      editForm.appendChild(label);
+    });
   } else if (info === 'award') {
     createTextInput(editForm, 'content', '', getData.content);
     createTextInput(editForm, 'organization', '', getData.organization);
