4fe77fe54c6b023f55054a76d45ed1bedae60d95
[AI10] 김민석 <phoves@naver.com>
2024-05-09 02:46:19 +0900

    footer하단고정

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,14 +1,7 @@
-// 새로운 <nav> 요소 생성
-var newElement = document.createElement("nav");
-newElement.id = "underbar";
-
 // <p> 요소 생성
 var copyrightP = document.createElement("p");
 copyrightP.textContent = "Copyright (c) 2024 PorShaW.";
 copyrightP.id = "footer"
 
-// <p> 요소를 <nav>에 추가
-newElement.appendChild(copyrightP);
 
-
-document.querySelector("#main").insertAdjacentElement("afterend", newElement);
+document.querySelector("#main").insertAdjacentElement("afterend", copyrightP);
