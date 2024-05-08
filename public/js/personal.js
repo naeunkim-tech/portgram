da1345fe1702d41ef68c95e60677f589cb931a6a
[AI10] 임지영 <dlawldud021@naver.com>
2024-05-08 23:55:09 +0900

    pageLoad.js로 모듈화

--- public/js/personal.js
+++ public/js/personal.js
@@ -2,51 +2,32 @@ import { addEdu } from './personal/addEdu.js';
 import { addAward } from './personal/addAward.js';
 import { addProject } from './personal/addProject.js';
 import { addCertificate } from './personal/addCertificate.js';
-import { postData } from './personal/postData.js';
 import { fetchMypageData } from './personal/fetchMypage.js';
+import { pageLoad } from './personal/pageLoad.js';
 
+// 초기 개인페이지
 document.addEventListener('DOMContentLoaded', () => {
   fetchMypageData();
 });
-
+// 학력
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('addEdu').addEventListener('click', addEdu);
-
-  const form = document.getElementById('educationForm');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    postData('educationForm', 'addedEducation', 'education');
-  });
+  pageLoad('addEdu', addEdu, 'educationForm', 'addedEducation', 'education');
 });
-
+// 수상이력
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('addAward').addEventListener('click', addAward);
-
-  const form = document.getElementById('awardForm');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    postData('awardForm', 'addedAward', 'award');
-  });
+  pageLoad('addAward', addAward, 'awardForm', 'addedAward', 'award');
 });
-
+// 프로젝트
 document.addEventListener('DOMContentLoaded', () => {
-  document
-    .getElementById('addCertificate')
-    .addEventListener('click', addCertificate);
-
-  const form = document.getElementById('certificateForm');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    postData('certificateForm', 'addedCertificate', 'certification');
-  });
+  pageLoad('addProject', addProject, 'projectForm', 'addedProject', 'project');
 });
-
+// 자격증
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('addProject').addEventListener('click', addProject);
-
-  const form = document.getElementById('projectForm');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    postData('projectForm', 'addedProject', 'project');
-  });
+  pageLoad(
+    'addCertificate',
+    addCertificate,
+    'certificateForm',
+    'addedCertificate',
+    'certification'
+  );
 });
