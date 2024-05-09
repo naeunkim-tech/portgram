30dad4f5ad309987a5c51af5c29c4166deb63641
[AI10] 김민석 <phoves@naver.com>
2024-05-09 15:05:11 +0900

    헤더/otheruser 배경색 수정

--- public/js/otheruser.js
+++ public/js/otheruser.js
@@ -4,6 +4,7 @@ const education_list = document.querySelector("#education_list");
 const award_list = document.querySelector("#award_list");
 const project_list = document.querySelector("#project_list");
 const certificate_list = document.querySelector("#license_list");
+const title = document.querySelector("title")
 
 const url = new URL(window.location.href)
 const userId = url.pathname.slice(11)
@@ -21,6 +22,7 @@ const fetchData = async () => {
     let award_output = "";
     let project_output = "";
     let certificate_output = "";
+    let title_name = "이름";
 
     edu_data.forEach((data) => {
         edu_output = edu_output + `<li>${data.school} | ${data.major} | ${data.degree}</li> `;
@@ -39,6 +41,7 @@ const fetchData = async () => {
     award_list.innerHTML = award_output;
     project_list.innerHTML = project_output;
     certificate_list.innerHTML = certificate_output;
+    title.innerHTML = `${title_name} - Portgram`
     console.log("update success")
 };
 
